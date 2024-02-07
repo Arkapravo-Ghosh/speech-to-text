398769ef120ab0a415682c6438638b7a6ad36324
Devendra Parihar <54232149+Devparihar5@users.noreply.github.com>
2024-02-07 06:44:11 +0000

    fix: gpu memory error

--- app/utils/transcribe.py
+++ app/utils/transcribe.py
@@ -9,8 +9,9 @@ async def transcribe(audio_content_path):
         # Check if the CACHE_DIR environment variable is set
         cache_dir_exists = False
         cache_dir = os.getenv("CACHE_DIR")
-        model_id = "openai/whisper-large-v3"
-
+    
+        model_id = "openai/whisper-tiny.en"
+        
         if cache_dir:
             cache_dir_exists = True
 
@@ -22,12 +23,12 @@ async def transcribe(audio_content_path):
 
         # Load the model
         if cache_dir_exists:
-            model = WhisperForConditionalGeneration.from_pretrained(
-                model_id, cache_dir=cache_dir
-            )
-
+            model = WhisperForConditionalGeneration.from_pretrained(model_id, cache_dir=cache_dir)
         else:
             model = WhisperForConditionalGeneration.from_pretrained(model_id)
+
+        # Move model to CPU
+        model.to("cpu")
 
         # Model configuration
         forced_decoder_ids = processor.get_decoder_prompt_ids(
@@ -36,10 +37,10 @@ async def transcribe(audio_content_path):
 
         model.config.forced_decoder_ids = forced_decoder_ids
 
-        # Converting the audio file to a wavform
+        # Converting the audio file to a waveform
         wavform, sr = librosa.load(audio_content_path, sr=16000)
 
-        # Creating a dataset from the wavform
+        # Creating a dataset from the waveform
         audio_dataset = Dataset.from_dict(
             {
                 "audio": [
@@ -59,6 +60,9 @@ async def transcribe(audio_content_path):
             sampling_rate=sample["sampling_rate"],
             return_tensors="pt",
         ).input_features
+
+        # Move input_features to CPU
+        input_features = input_features.to("cpu")
 
         predicted_ids = model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
         transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)[
